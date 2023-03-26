6e583872fee1f6952698f0433704da613d911654
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-03-26 20:01:40 +0300

    added Roboto font

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<!--<h1>This is an about page</h1>-->
+		<div>Test task for V-Jet</div>
+		<div>candidate</div>
+		<div>Alexander Lyakhov</div>
+	</div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style lang="scss" scoped>
+.about {
+	color: $text-200;
+	background: $bg-800;
+	text-align: center;
+	// letter-spacing: 1px;
+	padding: .75rem;
+
+	div {
+		font-size: 1.5rem;
+		padding: .5rem 0;
+	}
 }
 </style>
