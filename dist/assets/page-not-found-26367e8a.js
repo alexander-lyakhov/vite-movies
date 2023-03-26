cfc0faca6c77f53366ac4d74a341d1f75e4da64c
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-03-26 23:52:05 +0300

    dist subtree commit

---
+++ dist/assets/page-not-found-26367e8a.js
@@ -0,0 +1,1 @@
+import{_ as e,o,b as n}from"./index-0a877dde.js";const t={},c={class:"warning"};function s(a,r){return o(),n("div",c," Page not found ")}const d=e(t,[["render",s]]);export{d as default};
