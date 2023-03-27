c6fa13d7c2d88ddfb4fdff3e3a670ef6114cd408
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-03-27 09:56:46 +0300

    ...

--- dist/assets/page-not-found-3b18b1d0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,b as n}from"./index-61118e6d.js";const t={},c={class:"warning"};function s(a,r){return o(),n("div",c," Page not found ")}const d=e(t,[["render",s]]);export{d as default};
