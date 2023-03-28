b5ffe82eeef9d83604dec6bc91dce8666f3f9cc0
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-03-28 21:47:17 +0300

    left menu for sessions page

--- src/views/sessions.vue
+++ src/views/sessions.vue
@@ -3,38 +3,40 @@
   <section class="sessions">
     <div class="menu">
       <ul class="menu-body">
-        <li class="menu-item">Spirit</li>
-        <li class="menu-item">Spirit</li>
-        <li class="menu-item">Spirit</li>
-        <li class="menu-item">Spirit</li>
+        <li class="menu-item" v-for="item in sessions" :key="item.id">
+          <a href="#">{{ item.name }}</a>
+        </li>
       </ul>
     </div>
-    <div
-      class="movie"
-      v-for="movie in sessions"
-      :key="movie.id"
-    >
-      <poster
-        :image="movie.image"
-        :title="movie.name"
-        @click="showMovieInfo(movie.id)"
-      />
+    <div>
+      <div
+        class="movie"
+        v-for="movie in sessions"
+        :key="movie.id"
+      >
+        <poster
+          :image="movie.image"
+          :title="movie.name"
+          @click="showMovieInfo(movie.id)"
+        />
 
-      <div class="session-wrapper">
-        <row-container
-          v-for="(session, iDate) in movie.sessions"
-          :key="'${movie.id}_${iDate}'"
-          :title="new Date(session.showdate).toDateString()"
-          :items="session.daytime.split(';')"
-          v-slot="{item: time}"
-        >
-          <div
-            class="time"
-            @click="bookTickets(movie.id, session.showdate, time)"
+        <div class="session-wrapper">
+          <row-container
+            v-for="(session, iDate) in movie.sessions"
+            :key="'${movie.id}_${iDate}'"
+            :title="new Date(session.showdate).toDateString()"
+            :items="session.daytime.split(';')"
+            :itemWidth="64"
+            v-slot="{item: time}"
           >
-            {{ time }}
-          </div>
-        </row-container>
+            <div
+              class="time"
+              @click="bookTickets(movie.id, session.showdate, time)"
+            >
+              {{ time }}
+            </div>
+          </row-container>
+        </div>
       </div>
     </div>
   </section>
@@ -66,24 +68,39 @@
 .sessions {
   @extend .grid-container;
   grid-template-columns: 200px 1fr;
+  position: relative;
 
   .menu {
-    // position: fixed;
-    background: #088;
+    position: sticky;
+    top: .75rem;
+    align-self: start;
 
     &-body {
       background: $bg-600;
       padding: 4px;
-      // align-self: start;
     }
 
     &-item {
       background: $bg-800;
-      padding: .75rem .5rem;
-      cursor: pointer;
-  
+      height: 2.5rem;
+      line-height: 2.5rem;
+
       &:not(:last-child) {
         margin: 0 0 1px;
+      }
+
+      a {
+        display: block;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        width: 100%;
+        height: 100%;
+        padding: 0 .5rem;
+
+        &:hover {
+          background: $bg-700;
+        }
       }
     }
   }
