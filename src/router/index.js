4a629debb8b457a4c764da5c975eb8276da16a13
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-04-08 21:35:36 +0300

    use WebHashHistory mode for router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory  } from 'vue-router'
 import home from '@/views/home.vue'
 import movie from '@/views/movie'
 import movieInfo from '@/views/movie/info.vue'
 import movieSessions from '@/views/movie/sessions.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
